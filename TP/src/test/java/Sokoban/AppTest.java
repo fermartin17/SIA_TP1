4d6358f5c48849cb581e1b4f0d6aff5784693765
fermartin17 <femartin@itba.edu.ar>
2020-08-08 15:32:35 -0300

    Test examples

--- TP/src/test/java/Sokoban/AppTest.java
+++ TP/src/test/java/Sokoban/AppTest.java
@@ -1,38 +1,21 @@
 package Sokoban;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+   @Test
+    public void Example(){
+       assertEquals(2,2);
+   }
+
+    @Test
+    public void Example2(){
+        assertTrue(2 == 2);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
